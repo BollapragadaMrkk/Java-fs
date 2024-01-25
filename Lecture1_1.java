1d208643d3b4da1676e7f56df057a94cab72d885
BollapragadaMrkk <104060315+BollapragadaMrkk@users.noreply.github.com>
2024-01-25 14:34:45 +0530

    Update and rename As1.java to Lecture1_1.java

--- Lecture1_1.java
+++ Lecture1_1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class As1 {
+public class Lecture1_1{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter your Basic Pay:");
