ecbde67f7d410962d34ad2d17476a8e79a00beaf
BollapragadaMrkk <104060315+BollapragadaMrkk@users.noreply.github.com>
2024-01-25 14:32:57 +0530

    Update and rename Test.java to Lecture1.java

--- Lecture1.java
+++ Lecture1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Test{
+class Lecture1{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter any Number !");
